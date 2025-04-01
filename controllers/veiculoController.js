50644cc642480349d01c82cc25fcacecff6dbfcf
nathyyy06 <nathalia.cruz@estudante.ifms.edu.br>
2025-04-01 12:24:48 -0400

    31-03 continuação

--- controllers/veiculoController.js
+++ controllers/veiculoController.js
@@ -1,13 +1,29 @@
-const path = require("path")
+const path = require("path");
+
+const { PrismaClient } = require("@prisma/client");
+const client = new PrismaClient();
 
 class VeiculoController{
-static formCadastro(req,res) {
-    res.sendFile(path.join(__dirname,"../","views","formVeiculo.html"));
-}
+    static formCadastro(req, res) {
+        res.sendFile(path.join(__dirname, "../", "views", "formVeiculo.html"));
+    }
 
-static cadastrar(req,res) {}
+    static async cadastrar(req, res) {
+        res.send(JSON.stringify(req.body));
 
-static buscarTodos(req,res) {}
+        const {modelo, placa, ano, cor} = req.body
+
+        const veiculo = await client.veiculo.create({data: {
+            modelo,
+            placa,
+            ano: parseInt(ano),
+            cor
+        }});
+
+        res.send({veiculo});
+    }
+
+    static buscarTodos(req, res) {}
 }
 
 module.exports = VeiculoController;
---
+++ prisma/migrations/20250401162246_create_veiculo_model/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Veiculo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "modelo" TEXT NOT NULL,
+    "placa" TEXT NOT NULL,
+    "cor" TEXT NOT NULL,
+    "ano" INTEGER NOT NULL
+);
